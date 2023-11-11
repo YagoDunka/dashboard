506184f650e32ae83679617b1f4233c5f9ff452f
Yago Dunka <yagofdunka@gmail.com>
2023-11-11 09:19:26 -0300

    Requirements

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
+import numpy as np
 
 app = Flask(__name__)
+
+lista = np.array([1, 4, 7, 10, 34, 23, 4, 6, 2])
+
+idx = np.where(lista < 7)
 
 @app.route('/')
 
 def home():
-    return "Hello, World!"
+    return str(lista[idx])
 
 if __name__ == '__main__':
     app.run()
